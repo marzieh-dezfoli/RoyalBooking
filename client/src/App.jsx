62e8c34456d7037a192be862ae08ae5af0c6e4ca
Marzieh Lorzade Dezfoli <marziehlorzadedezfoli@gmail.com>
2025-06-15 02:49:36 -0700

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { Route, Routes,useLocation } from 'react-router-dom';
+import Home from './pages/Home';
+
+
+const App = () => {
+  const location = useLocation();
+  const isOwnerPath = location.pathname.includes("owner");
+
+  return (
+    <div>
+      { !isOwnerPath && <Navbar /> }
+      <div className='min-h-[70vh]'>
+        <Routes>
+<Route path='/' element={<Home/>} />
+
+        </Routes>
+
+      </div>
+    </div>
+  );
+};
+
+export default App;
